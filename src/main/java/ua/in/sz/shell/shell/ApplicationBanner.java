d7d473664681f46adafc725c20599bdb72bfd953
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-15 23:50:58 +0200

    Create base spring shell

---
+++ src/main/java/ua/in/sz/shell/shell/ApplicationBanner.java
@@ -0,0 +1,19 @@
+package ua.in.sz.shell.shell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
+
+@Slf4j
+public class ApplicationBanner implements Banner {
+	@Override
+	public void printBanner(Environment env, Class<?> sourceClass, PrintStream out) {
+		out.println(AnsiOutput.toString(AnsiColor.GREEN, "tools", AnsiColor.DEFAULT, " ", AnsiStyle.FAINT, "1.0"));
+		out.println();
+	}
+}
