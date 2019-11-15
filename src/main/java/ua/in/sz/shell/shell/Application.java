6284d7e6475cdfb7a71cc6f7bddbe453acc1a67d
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-16 00:25:44 +0200

    Create base spring shell

--- src/main/java/ua/in/sz/shell/shell/Application.java
+++ src/main/java/ua/in/sz/shell/shell/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShellApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(ShellApplication.class);
+		SpringApplication app = new SpringApplication(Application.class);
 		app.setBanner(new ApplicationBanner());
+		app.setLogStartupInfo(false);
 		app.run(args);
 	}
 
