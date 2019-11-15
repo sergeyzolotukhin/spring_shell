fca3d11482f198760670b245d9633e432184da30
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-15 22:36:36 +0200

    Create base spring shell

---
+++ src/main/java/ua/in/sz/shell/shell/ShellApplication.java
@@ -0,0 +1,13 @@
+package ua.in.sz.shell.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellApplication.class, args);
+	}
+
+}
