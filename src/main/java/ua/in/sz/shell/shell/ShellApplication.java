ab6e14426164e8db10709461517fff8166a61c94
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-15 23:47:03 +0200

    Create base spring shell

--- src/main/java/ua/in/sz/shell/shell/ShellApplication.java
+++ src/main/java/ua/in/sz/shell/shell/ShellApplication.java
@@ -1,13 +1,29 @@
 package ua.in.sz.shell.shell;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class ShellApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShellApplication.class, args);
+		SpringApplication app = new SpringApplication(ShellApplication.class);
+		app.setBanner(new Banner() {
+			@Override
+			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream printStream) {
+				printStream.println(AnsiOutput.toString(AnsiColor.GREEN, "tools", AnsiColor.DEFAULT, " ", AnsiStyle.FAINT, "1.0"));
+				printStream.println();
+			}
+		});
+		app.run(args);
 	}
 
 }
