204424a35fbf8aca6e91723316843225cac2dff4
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-16 01:45:19 +0200

    Create base spring shell

--- src/main/java/ua/in/sz/shell/shell/command/UserCommand.java
+++ src/main/java/ua/in/sz/shell/shell/command/UserCommand.java
@@ -1,6 +1,9 @@
 package ua.in.sz.shell.shell.command;
 
 import org.jline.reader.LineReader;
+import org.jline.terminal.Terminal;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -9,17 +12,33 @@ import org.springframework.shell.standard.ShellMethod;
 public class UserCommand {
 
 	private final LineReader lineReader;
+	private final Terminal terminal;
 
 	@Lazy
-	public UserCommand(LineReader lineReader) {
+	public UserCommand(LineReader lineReader, Terminal terminal) {
 		this.lineReader = lineReader;
+		this.terminal = terminal;
 	}
 
 	@ShellMethod("Add user.")
 	public String add() {
-		String name = lineReader.readLine(String.format("name [%s]:", "root"));
+		print(red("Hi"));
+
+		String name = lineReader.readLine(red(String.format("name [%s]:", "root")));
 		String password = lineReader.readLine(String.format("password [%s]:", "root"));
 
 		return String.format("create user [%s/%s]", name, password);
+	}
+
+	private String red(String message) {
+		AttributedStringBuilder attr = new AttributedStringBuilder();
+		AttributedStyle fg = AttributedStyle.DEFAULT.foreground(AttributedStyle.RED);
+
+		return attr.append(message, fg).toAnsi();
+	}
+
+	private void print(String text) {
+		terminal.writer().println(text);
+		terminal.flush();
 	}
 }
